e7a76a47c119215036957bad83a2866dbc32a0c2
Elf <vet.msn@gmail.com>
2022-05-16 22:04:10 +0500

    Условия

--- script.js
+++ script.js
@@ -11,11 +11,39 @@ const personalMovieDB = {
 };
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?'),
+      b = prompt('На сколько оцените его?', ''),
       c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?');
+      d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+
+
+// if (num < 49) {
+//     console.log("Error");
+// } else if (num > 100) {
+//     console.log("To Much");
+// } else {
+//     console.log('Ok!');
+// }
+
+const num = 50;
+
+switch(num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
+
